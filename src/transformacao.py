dbbb887a9d87f40e8df6fed2d33c28dd4923c35c
Luciano Arruda <lucasards32@gmail.com>
2026-08-11 22:48:51 -0300

    construção do modulo transformação aonde recebo o json produzido pela extração e transformo em dataframes

---
+++ src/transformacao.py
@@ -0,0 +1,47 @@
+import logging
+
+import pandas as pd
+
+# Logging
+
+logger = logging.getLogger(__name__)
+
+## Transformação:
+
+def transform(dados):
+    
+    logger.info("Iniciando a transformação dos dados.")
+    
+    df = pd.Dataframe(dados)
+    
+    logger.info(
+        "Dataframe criado em %s linhas e %s colunas",
+        df.shape[0],
+        df.shape[1]
+    )
+    df = df.rename(
+         columns={
+             "D1N": "localidade",
+            "D2N": "periodo",
+            "D3N": "indicador",
+            "V": "valor"
+         }
+     )
+    logger.info("Colunas renomeadas com sucesso!")
+    
+    colunas = [
+        "localidade",
+        "periodo",
+        "indicador",
+        "valor"
+    ]
+    df = df[colunas]
+    
+    df["valor"] = pd.to_numeric(
+        df["valor"],
+        errors="coerce"
+    )
+    logger.info(
+        "Transformação concluída com sucesso"
+    )
+    return df
